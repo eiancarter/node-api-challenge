db747826df7fecf789b977a03491ef99e68727a6
Eian Carter <eian@bricleir.com>
2020-02-14 09:06:35 -0800

    finished mvp but trying to troubleshoot server error

---
+++ data/helpers/actionRouter.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const Actions = require("./actionModel");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    const pagination = req.query;
+    Actions.get(pagination)
+        .then(actions => {
+            res.status(200).json(actions)
+        })
+        .catch(error => {
+            console.log(error)
+            res.status(500).json({ message: "error retrieving actions" })
+        })
+});
+
+router.get("/:id", (req, res) => {
+    Actions.get(req.params.id)
+        .then(action => {
+            if(action) {
+                res.status(200).json(action);
+            } else {
+                res.status(404).json({ message: "action not found" })
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({
+                message: "error retrieving that action"
+            })
+        })
+});
+
+router.delete("/:id", (req, res) => {
+    Actions.remove(req.params.id)
+        .then(count => {
+            if(count > 0) {
+                res.status(200).json({ message: "the action is gone" })
+            } else {
+                res.status(404).json({ message: "cannot delete action" })
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({ message: "error deleting" })
+        })
+});
+
+router.put("/:id", (req, res) => {
+    const changes = req.body;
+    Actions.update(req.params.id, changes)
+        .then(action => {
+            if(action) {
+                res.status(200).json(action);
+            } else {
+                res.status(404).json({ message: "the action cannot be changed" })
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({ message: "error updating" })
+        });
+});
+
+
+module.exports = router;

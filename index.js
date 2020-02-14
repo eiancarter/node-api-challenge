7dc0c75780107c7d15408d03df471ebd1c6b9efa
Eian Carter <eian@bricleir.com>
2020-02-14 09:26:42 -0800

    got server working debuggin get req

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ Go code!
 const server = require("./server");
 
 // const port = process.env.PORT || 5000;
-const port = 5000;
+const port = 5600;
 
 server.listen(port, () => {
     console.log(`server is running on localhost:${port}`)
